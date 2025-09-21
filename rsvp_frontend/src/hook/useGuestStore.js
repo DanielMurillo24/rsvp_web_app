f8a6e0fcebad278b2442b871a811491815b7989b
Daniel Murillo <jose.murillo@ucrso.info>
2025-09-21 10:01:57 -0600

    Updating variable guest to follow consistency in all the dispatch functions

--- rsvp_frontend/src/hook/useGuestStore.js
+++ rsvp_frontend/src/hook/useGuestStore.js
@@ -17,7 +17,7 @@ export const useGuestStore = () => {
       localStorage.setItem("token", data.token);
       localStorage.setItem("token-init-date", new Date().getTime());
 
-      dispatch(onLogin({ token: data.token, invitado: data.invitado }));
+      dispatch(onLogin({ token: data.token, guest: data.invitado }));
     } catch (error) {
       if (error.response) {
         // Si el backend responde con 401, es un error de autenticación real
